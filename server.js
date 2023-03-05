b72ff602d160e1bfa32bfc2ffd29392824f503ca
Ioannis Kyriazis <ikyriazis@wpi.edu>
2023-03-04 20:36:39 -0500

    up and running

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
  // Configure & Run the http server
  const app = express();
 
- app.use(express.static(__dirname ));
+ app.use(express.static(__dirname + '/public'));
 
  app.listen(5001, () => {
-   console.log('HTTP server running on port 7000');
+   console.log('HTTP server running on port 5001');
    });
